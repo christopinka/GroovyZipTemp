ce89db4c9fbe57e98249de6e6d4597f12aaa64c7
Chris Topinka <christopinka@Chris-Topinkas-Mac-mini.local>
2012-10-28 21:22:37 -0500

    added complex conditional to location query

--- README.md
+++ README.md
@@ -2,7 +2,11 @@ Answer to Ruby Quiz #68 - http://www.rubyquiz.com/quiz68.html
 but in Groovy!
 
 Run 
+
+
 <code>
     $ groovy temperature.groovy \<zip code\>
 </code>
+
+
 to get the temperature for any city corresponding to a 5 digit US zip code.
--- temperature.groovy
+++ temperature.groovy
@@ -1,6 +1,5 @@
 import groovy.json.*
 
-// Must have arg
 if (args.size() != 1){
 	println "We'll need a valid US zip code. Thanks."
         return
@@ -9,7 +8,6 @@ if (args.size() != 1){
 def zipCode = args.getAt(0)
 def validZipPattern = /\d{5}/
 
-//Arg matches pattern
 if(!zipCode.matches(validZipPattern)){
 	println 'Double check your zip code. Only 5 digit zip codes are allowed. Sorry.'
 	return
@@ -24,29 +22,13 @@ def geoCodeResponse = new URL(url).text
 def locations = new JsonSlurper().parseText(geoCodeResponse)
 
 //println locations
-
-//Response not null, check status field
-//Todo locations could be null and  status could be null or not there, wrap accordingly
 if(locations.status != 'OK'){
 	println "Doouble check your zip code. This zip code wasn't found. Sorry."
 	return
 }
 
-def location = locations?.results?.find{ it.address_components?.long_name?.find{ country-> country.equals('United States') } }
-
-
-//def foo 
-//foo = ['bar', 'baz']
-//assert "bar" == foo?.find{true}
-	//Otherwise take the first US result in the list of possibly ambiguous results and avoid out of bounds if empty
-
-def address = location?.formatted_address
-
-	//Was address found?
-if(address == null){
-	println "No address information was found for zip code ${zipCode}."
-	return
-}
+def location = locations?.results?.find{ it.address_components?.long_name?.find{ country-> country.equals('United States')}\
+	 && it.address_components?.types.find{ types-> types.contains('country') } }
 
 def wunderGroundConditionsRequest = "http://api.wunderground.com/api/3df1783a4eab5c53/geolookup/conditions/q/${zipCode}.json"
 def wunderGroundConditionsResponse = new URL(wunderGroundConditionsRequest).text
@@ -58,8 +40,9 @@ if(weather.response.error){
   	println "${weather.response.error.description} of zip code ${zipCode}."
 	return
 }
+
 println """\
-	The current temperature for ${address} is  
+	The current temperature for ${location?.formatted_address?:zipCode} is  
 		${weather?.current_observation?.temp_f} degrees fahrenheit. 
 		Please, enjoy your day.
  """
